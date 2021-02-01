a2a331b7ba365539999f8bbcbcb3a4d7fcda08af
3unjung <margaux.aclo1941@gmail.com>
2021-02-01 21:27:57 +0100

    rework form and uploading files

--- jarditou.py
+++ jarditou.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, config, request
+from random import randint
+from flask import Flask, render_template, config, request, url_for
 from flask_ngrok import run_with_ngrok
 from database import connexionMysql
+from flask import request, redirect
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -41,20 +45,28 @@ def produit(libelle):
 def newProduct():
     sqlConnection = connexionMysql()
     cursor = sqlConnection.cursor(dictionary=True)
-    cursor.execute(f"SELECT * from categories")
+    cursor.execute(f"SELECT * from produits")
     results = cursor.fetchall()
+
+    cursor.execute("SELECT * from categories")
+    categories = cursor.fetchall()
 
     if request.method == "POST":
         libelle = request.form["libelle"]
-        reference = request.form["reference"]
         category = int(request.form["categorie"])
+        couleur = request.form["couleur"]
         prix = request.form["price"]
         quantite = request.form["quantite"]
+        reference = (''.join([mot[0] for mot in libelle.split(" ") + couleur.split(" ")]) + str(randint(10000, 99999))).upper()
+        f = request.files["file"]
+        f.save(os.path.join(app.config['IMAGE-UPLOADS'], f.filename))  # filename = nom du fichier complet
         cursor = sqlConnection.cursor()
-        cursor.execute(f"INSERT INTO produits (pro_libelle, pro_ref, pro_cat_id, pro_prix, pro_stock) VALUES ('{libelle}', '{reference}', '{category}', '{prix}', '{quantite}');")
+        cursor.execute(
+            f"INSERT INTO produits (pro_libelle, pro_ref, pro_cat_id, pro_couleur, pro_prix, pro_stock, pro_photo) "
+            f"VALUES ('{libelle}','{reference}', '{category}', '{couleur}', '{prix}', '{quantite}', '{f.filename}');")
         sqlConnection.commit()
-        return render_template("views/catalogue.html", results=results)
-    return render_template("views/addproduct.html", results=results)
+        return redirect(url_for('catalogue'))
+    return render_template("views/addproduct.html", results=results, categories=categories)
 
 
 @app.route("/inscription")
@@ -85,8 +97,31 @@ def treatment():
 # def page_not_found(error):
 #     return render_template('views/404.jarditou.html'), 404
 
+# Route test version
+@app.route("/test")
+def itz():
+    return render_template("views/test.html")
+
+
+@app.route("/upload", methods=["post", "get"])
+def upld():
+    if request.method == "POST":
+        f = request.files['file']
+
+        print(f)
+
+        f.save(os.path.join(app.config['IMAGE-UPLOADS'], f.filename))
+
+        print("succes")
+
+        return redirect(request.url)
+
+    return render_template("views/upload.html")
+
 
 if __name__ == '__main__':
     app.config['DEBUG'] = True
     app.config['TESTING'] = True
+    app.config['IMAGE-UPLOADS'] = "D:\Documents\Dropbox\jarditou\static\img"  # spécifie l'emplacement des fichiers à
+    # télécharger
     app.run()
